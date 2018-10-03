d92f58eb054cdf8d5305c6b7e94c0bb3b814a91f
Ordolph <austin.t.randolph@gmail.com>
2018-10-03 17:41:56 -0400

    Adjusted formatiing of js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,109 +1,77 @@
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
-
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-
 var wins = document.getElementById("wins")
-
 var losses = document.getElementById("losses")
-
 var remGuesses = document.getElementById("remGuesses")
-
 var guesses = document.getElementById("guesses")
-
 var winsNum = 0;
-
 var lossNum = 0;
-
 var remGuessNum = 8;
-
 var guessLetter = [];
 
 wins.textContent = "Wins: " + winsNum;
-
 losses.textContent = "Losses: " + lossNum;
-
 remGuesses.textContent = "Remaining Guesses: " + remGuessNum;
-
 guesses.textContent = "Your Guesses: " + guessLetter;
 
-console.log (randomLetter)
+console.log(randomLetter)
 
-document.onkeyup = function game (start) {
+document.onkeyup = function game(start) {
 
     var userGuess = start.key;
 
     function win() {
-    
-        winsNum++ ; 
-            
+
+        winsNum++;
         wins.textContent = "Wins: " + winsNum;
-    
         randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    
         remGuessNum = 8;
-    
         remGuesses.textContent = "Remaining Guesses: " + remGuessNum;
-
         guessLetter = []
-
         guesses.textContent = "Your Guesses: " + guessLetter;
-    
+
         console.log(randomLetter)
-    
         console.log(remGuesses.textContent)
     }
-    
+
     function lose() {
-    
-        lossNum ++;
-    
+
+        lossNum++;
         losses.textContent = "Losses: " + lossNum;
-    
         randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    
         remGuessNum = 8;
-    
         remGuesses.textContent = "Remaining Guesses: " + remGuessNum;
-
         guessLetter = []
-
         guesses.textContent = "Your Guesses: " + guessLetter;
-    
-        console.log (randomLetter)
+
+        console.log(randomLetter)
     }
-    
+
     function wrongGuess() {
-    
-        remGuessNum--;
-    
-        remGuesses.textContent = "Remaining Guesses: " + remGuessNum;
-    
-        guessLetter.push(start.key);
 
+        remGuessNum--;
+        remGuesses.textContent = "Remaining Guesses: " + remGuessNum;
+        guessLetter.push(start.key);
         guesses.textContent = "Your Guesses: " + guessLetter;
     }
 
-    if (alphabet.includes(event.key)){
+    if (alphabet.includes(event.key)) {
 
         if (randomLetter == userGuess) {
 
             win();
-
         }
 
         else if (remGuessNum == 0) {
 
             lose()
-
-
         }
 
         else {
 
             wrongGuess()
-
-        
-}}
-console.log(userGuess)
+        }
+    }
+    console.log(userGuess)
 
 }
