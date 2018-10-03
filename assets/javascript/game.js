a66d3c22626f5ec6002c8231187a11efb8c35dce
Ordolph <austin.t.randolph@gmail.com>
2018-10-03 14:38:13 -0400

    Added initial files for project, wrote html for index page, and started on javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+var randomLetter = getRandomLetter;
+
+console.log (randomLetter) 
